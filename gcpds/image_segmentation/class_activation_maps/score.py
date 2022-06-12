a6a490c496e345caab78ab5ad85e5df4e9e9cb9d
aguirrejuan <jucaguirrear@unal.edu.co>
2022-06-12 14:54:26 -0500

    Fixed dimentions for scorecam in score class

--- gcpds/image_segmentation/class_activation_maps/score.py
+++ gcpds/image_segmentation/class_activation_maps/score.py
@@ -4,13 +4,14 @@ Score for semantic segmentation models to use tf-keras-vis.
 
 
 import tensorflow as tf
+import matplotlib.pyplot as plt 
 
 
 class SegScore:
     """
     Score for semantic segmentation models to use tf-keras-vis.
     """
-    def __init__(self, target_mask: tf.Tensor, class_channel: int =0) -> None:
+    def __init__(self, target_mask: tf.Tensor, class_channel: int = 0) -> None:
         self.target_mask = self.__sparse(target_mask)
         self.class_channel = class_channel
 
@@ -22,15 +23,15 @@ class SegScore:
 
         class_pred = pred
         if channels != 1:
-            class_pred = pred[...,self.class_channel]
+            class_pred = class_pred[...,self.class_channel]
             class_pred = class_pred[...,None]
         else: 
             if self.class_channel == 0:
                 class_pred = 1-class_pred
-
+        
         masked_scores = class_mask*class_pred
-        mean_scores = tf.reduce_mean(masked_scores,axis=[1,2,3])
-        return mean_scores[...,None]
+        mean_scores = tf.reduce_mean(masked_scores, axis=[-1,-2,-3])
+        return mean_scores
 
     @staticmethod
     def __sparse(masks: tf.Tensor) -> tf.Tensor:
@@ -39,8 +40,3 @@ class SegScore:
             return masks 
         sparse_mask = tf.argmax(masks,axis=-1)
         return sparse_mask[...,None]
-
-
-
-
-
