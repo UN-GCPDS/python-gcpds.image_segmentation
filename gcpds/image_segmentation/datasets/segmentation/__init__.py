83709c16782c2bc5437c7dd8eeb6ad1352a5d968
Camilo.pelaez <cpelaezg@unal.edu.co>
2023-03-16 23:49:30 -0500

    Add a new dataset and the new models to __init__ scripts

--- gcpds/image_segmentation/datasets/segmentation/__init__.py
+++ gcpds/image_segmentation/datasets/segmentation/__init__.py
@@ -4,3 +4,4 @@ from .ultrasound_nerve_brachial_plexus import BrachialPlexus
 from .oxford_iiit_pet import OxfordIiitPet
 from .infrared_thermal_feet import InfraredThermalFeet
 from .tomato_seeds import TomatoSeeds
+from .zeamays_seeds import ZeaMaysSeeds
--- gcpds/image_segmentation/models/__init__.py
+++ gcpds/image_segmentation/models/__init__.py
@@ -2,3 +2,5 @@ from .baseline_fcn import fcn_baseline
 from .baseline_unet import unet_baseline
 from .baseline_segnet import segnet_baseline
 from .baseline_res_unet import res_unet_baseline
+from .mobilenetv2_unet import unet_mobilenet
+from .vgg16_unet import unet_vgg16
