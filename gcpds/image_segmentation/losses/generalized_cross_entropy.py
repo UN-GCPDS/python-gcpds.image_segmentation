4d958713cd47aee9ecc46be2fe803b2891361ec8
aguirrejuan <jucaguirrear@unal.edu.co>
2023-03-21 14:55:15 -0500

    Update-fixed: Generalized Cross-entropy

--- gcpds/image_segmentation/losses/generalized_cross_entropy.py
+++ gcpds/image_segmentation/losses/generalized_cross_entropy.py
@@ -8,21 +8,20 @@ from tensorflow.keras.losses import Loss
 from tensorflow.keras import backend as K
 
 class GeneralizedCrossEntropy(Loss):
-    def __init__(self, q=1, name='GeneralizedCrossEntropy', **kwargs):
+    def __init__(self, q=1, name='GeneralizedCrossEntropy',**kwargs):
         self.q = q
         super().__init__(name=name,**kwargs)
 
     def call(self, y_true, y_pred):
         haddamard = y_true*y_pred
-        haddamard_sum = K.sum(haddamard,axis=[1,2,3]) # batch_size, value
-        
-        results = 2 * (1-K.pow(haddamard_sum,self.q))/self.q
+        haddamard_sum = K.sum(haddamard, axis=[-1]) # batch_size, h, w
+        results = 2 *(1 - K.pow(haddamard_sum,self.q))/self.q
+        results = K.mean(results, axis=[1,2]) # bath_size, value 
         return results 
-
+    
     def get_config(self,):
         base_config = super().get_config()
         return {**base_config, "q": self.q}
-
 
 if __name__ == "__main__":
     import numpy as np 
